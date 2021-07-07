07ed842fee1a7e0586636e85e3d082a6d0a4239d
Arshad <you@example.com>
2021-07-07 16:42:43 +0530

    final code

--- signlanguage/speach.py
+++ signlanguage/speach.py
@@ -1,18 +1,22 @@
 import speech_recognition as sr
+import time
 r = sr.Recognizer()
+print("Speech Recognition is running... ")
 
 while(1):
     try:
+        time.sleep(2)
+        '''
         with sr.Microphone() as source2:
             r.adjust_for_ambient_noise(source2, duration=0.2)
             audio2 = r.listen(source2)
             MyText = r.recognize_google(audio2)
             MyText = MyText.lower()
             print("text is :",MyText)
-	    
-			
+        '''
+            
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
-		
+        
     except sr.UnknownValueError:
         print("unknown error occured")
--- signlanguage/start.py
+++ signlanguage/start.py
@@ -4,29 +4,33 @@ from time import *
 
 #lcd = lcddriver.lcd()
 GPIO.setwarnings(False) 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) 
-#lcd.lcd_display_string("		PROJECT", 1)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(10, GPIO.IN) 
+#lcd.lcd_display_string("       PROJECT", 1)
 #lcd.lcd_display_string("An Electronic Assistant", 2)
-#lcd.lcd_display_string("	For Deaf-Mute", 3)
+#lcd.lcd_display_string("   For Deaf-Mute", 3)
 sleep(3)
 #lcd.lcd_clear()
 
+
 while True:
-	button_state=GPIO.input(10) 
-	if(button_state==0):
-		#lcd.lcd_clear()
-		#lcd.lcd_display_string("	Gesture mode", 2)
-		#lcd.lcd_display_string("		Activated", 3)
-		sleep(2)
-		#lcd.lcd_clear()
-		import fun_util
-		fun_util.recognize()
-	else:
-		#lcd.lcd_clear()
-		#lcd.lcd_display_string("	Speech mode", 2)
-		#lcd.lcd_display_string("		Activated", 3)
-		sleep(2)
-		#lcd.lcd_clear() 
-		import speech 
-		speech.rec()
+    button_state=GPIO.input(10) 
+    if(button_state==0):
+        print("Gesture Mode")
+        import set_hand_hist
+        #lcd.lcd_clear()
+        #lcd.lcd_display_string("   Gesture mode", 2)
+        #lcd.lcd_display_string("       Activated", 3)
+        sleep(2)
+        #lcd.lcd_clear()
+        import fun_util
+        fun_util.recognize()
+    else:
+        print("Speach Mode")
+        #lcd.lcd_clear()
+        #lcd.lcd_display_string("   Speech mode", 2)
+        #lcd.lcd_display_string("       Activated", 3)
+        sleep(2)
+        #lcd.lcd_clear() 
+        import speach 
+        speech.rec()
